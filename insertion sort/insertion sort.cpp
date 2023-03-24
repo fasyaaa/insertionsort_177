71a60b2128207183856b2c66b8ee98c41b296ae0
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-24 10:30:39 +0700

    mengembalikan yang ke hapus

--- insertion sort/insertion sort.cpp
+++ insertion sort/insertion sort.cpp
@@ -1,1 +1,28 @@
-d
+#include <iostream>
+using namespace std;
+
+int arr[20];            //Membuat array dengan panjang data 20
+int n;                 //Membuat Variable inputan n
+
+void input() {          //Procedure Input
+    while (true) {
+        cout << "Masukkan Jumlah Data pada Array : ";           //Membuat Inputan jumlah element array
+        cin >> n;                                               //Memanggil variable inputan n
+
+        if (n <= 20) {                                          //Membuat kondisi n tidak lebih dari sama den 20
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n";        //Menampilkan Pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                                       //Membuat jarak per baris program
+    cout << "======================" << endl;           //Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
