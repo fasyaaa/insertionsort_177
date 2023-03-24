9b60363f0ebaadd12d59fbdce71073dfa8ceabf4
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-24 10:33:58 +0700

    menambah perulangan untuk array

--- insertion sort/insertion sort.cpp
+++ insertion sort/insertion sort.cpp
@@ -20,9 +20,10 @@ void input() {          //Procedure Input
     cout << "======================" << endl;           //Membuat tampilan susunan data element array
     cout << "Masukkan Element Array" << endl;
     cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke- " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
