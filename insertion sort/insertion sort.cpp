4e3fe080e1d9e292c864d06f4bda6c6f27658222
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-24 10:16:17 +0700

    membuat insertion

--- insertion sort/insertion sort.cpp
+++ insertion sort/insertion sort.cpp
@@ -1,7 +1,26 @@
-// insertion sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int arr[20];            //Membuat array dengan panjang data 20
+int n;                 //Membuat Variable inputan n
+
+void input() {          //Procedure Input
+    while (true) {
+        cout << "Masukkan Jumlah Data pada Array : ";           //Membuat Inputan jumlah element array
+        cin >> n;                                               //Memanggil vatiable inputan n
+
+        if (n <= 20) {                                          //Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n";        //Menampilkan Pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                                       //Membuat jarak per baris program
+    cout << "======================" << endl;           //Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+}
 
 int main()
 {
